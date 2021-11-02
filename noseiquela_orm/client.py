0f32661d374104e45ffa6098e11a0f486d9b74c2
Geraldo Castro <6025018+exageraldo@users.noreply.github.com>
2021-11-02 00:00:16 -0300

    Accept Meta class inside entity models to override datastore.Client instance params (#4)
    * Accept Meta class inside entity models to override datastore.Client instance params
    
    * Add types

--- noseiquela_orm/client.py
+++ noseiquela_orm/client.py
@@ -1,26 +1,45 @@
+from typing import Callable, Dict, Optional, Union
 from functools import partial
 
 from google.cloud import datastore
+from google.cloud.datastore.client import _CLIENT_INFO
+from google.cloud.datastore.entity import Entity as GoogleEntity
+from google.auth.credentials import Credentials as GoogleCredentials
+from google.api_core.gapic_v1.client_info import ClientInfo as GoogleClientIngo
+from google.api_core.client_options import ClientOptions as GoogleClientOptions
+from google.api_core.retry import Retry as GoogleRetry
+from requests import Session
 
 
 class DataStoreClient:
-    _instance = None
+    def __init__(self,
+        project:Optional[str]=None,
+        namespace:Optional[str]=None,
+        credentials:Optional[GoogleCredentials]=None,
+        client_info:Optional[GoogleClientIngo]=None,
+        client_options:Optional[GoogleClientOptions]=None,
+        _http:Optional[Session]=None,
+        _use_grpc:Optional[bool]=None
+    ) -> None:
+        self._project = project
+        self._namespace = namespace
+        self._client = datastore.Client(
+            project=project,
+            namespace=namespace,
+            credentials=credentials,
+            client_info=(client_info or _CLIENT_INFO),
+            client_options=client_options,
+            _http=_http,
+            _use_grpc=_use_grpc
+        )
 
-    def __init__(self) -> None:
-        self._client = datastore.Client()
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def _get_partial_query(self, kind):
+    def _get_partial_query(self, kind: Union[str, int]) -> Callable:
         return partial(
             self._client.query,
             kind=kind
         )
 
-    def save(self, entity, retry=None, timeout=None):
+    def save(self, entity:GoogleEntity, retry:Optional[GoogleRetry]=None, timeout:Optional[float]=None) -> Union[str, int]:
         self._client.put_multi(
             entities=[entity],
             retry=retry,
@@ -28,10 +47,20 @@ class DataStoreClient:
         )
         return entity.key.id
 
-    @property
-    def project(self):
-        return self._client.project
+    @staticmethod
+    def _mount_google_entity(entity_dict: Dict, key_case: Callable) -> GoogleEntity:
+        entity_key = entity_dict.pop("id")
+        entity = GoogleEntity(entity_key)
+        for key, value in entity_dict.items():
+            key_name = key_case(key)
+            entity[key_name] = value
+
+        return entity
 
     @property
-    def namespace(self):
-        return self._client.namespace
+    def project(self) -> str:
+        return self._project or self._client.project
+
+    @property
+    def namespace(self) -> str:
+        return self._namespace or self._client.namespace
