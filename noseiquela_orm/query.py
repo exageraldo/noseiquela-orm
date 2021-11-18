956a7da54c8d327772a30ebadd332620d6fba082
Geraldo Castro <victormatheuscastro@gmail.com>
2021-11-18 16:15:25 -0300

    [Geraldo] Add more type hints

--- noseiquela_orm/query.py
+++ noseiquela_orm/query.py
@@ -1,8 +1,20 @@
-from typing import Dict, Tuple
+from typing import Any, Dict, Tuple, Optional, Generator, TYPE_CHECKING
+from functools import partial
+from google.cloud.datastore.query import Query as GoogleQuery
 
+if TYPE_CHECKING:
+    from .entity import Entity
 
 class QueryResult:
-    def __init__(self, query, entity_instance, limit=None, offset=0, retry=None, timeout=None) -> None:
+    def __init__(
+        self,
+        query: GoogleQuery,
+        entity_instance: 'Entity',
+        limit: Optional[int]=None,
+        offset: Optional[int]=0,
+        retry: Optional[int]=None,
+        timeout: Optional[int]=None
+    ) -> None:
         self.query = query
         self.limit = limit
         self.offset = offset
@@ -10,7 +22,7 @@ class QueryResult:
         self.timeout = timeout
         self.entity_instance = entity_instance
 
-    def first(self):
+    def first(self) -> Optional['Entity']:
         result = list(self.query.fetch(
             limit=1,
             retry=self.retry,
@@ -20,7 +32,7 @@ class QueryResult:
             result[0]
         ) if result else None
 
-    def _get_query_result(self):
+    def _get_query_result(self) -> Generator['Entity', None, None]:
         for entity in self.query.fetch(
             limit=self.limit,
             offset=self.offset,
@@ -31,38 +43,67 @@ class QueryResult:
                 entity
             )
 
-    def __iter__(self):
+    def __iter__(self) -> Generator['Entity', None, None]:
         return self._get_query_result()
 
-    def __next__(self):
+    def __next__(self) -> Generator['Entity', None, None]:
         yield from self._get_query_result()
 
 
 class Query:
-    def __init__(self, partial_query, entity_instance) -> None:
+    def __init__(
+        self,
+        partial_query: partial,
+        entity_instance: 'Entity',
+    ) -> None:
         self.partial_query = partial_query
         self.entity_instance = entity_instance
 
-    def all(self, order_by=None, projection=None):
-        _query = self.partial_query(
-            order=(order_by or ()),
-            projection=(projection or ()),
-        )
-        return QueryResult(_query, self.entity_instance)
-
-    def filter(self, order_by=None, projection=None, distinct_on=None, parent_id=None, **kwargs):
-        filter_list = self._process_filters(kwargs)
-        _query = self.partial_query(
-            filters=filter_list,
-            ancestor=parent_id,
+    def _mount_query(
+        self,
+        filters: Optional[Tuple[str, str, Any]]=None,
+        order_by: Optional[Tuple[str]]=None,
+        projection: Optional[Tuple[str]]=None,
+        distinct_on: Optional[Tuple[str]]=None,
+        parent_id: Optional[Tuple[str]]=None
+    ) -> GoogleQuery:
+        return self.partial_query(
+            filters=(filters or ()),
+            ancestor=(parent_id or None),
             projection=(projection or ()),
             order=(order_by or ()),
             distinct_on=(distinct_on or ()),
         )
+
+    def all(
+        self,
+        order_by: Optional[Tuple[str]]=None,
+        projection: Optional[Tuple[str]]=None
+    ) -> 'QueryResult':
+        _query = self._mount_query(
+            order_by=order_by,
+            projection=projection
+        )
         return QueryResult(_query, self.entity_instance)
 
+    def filter(
+        self,
+        order_by: Optional[Tuple[str]]=None,
+        projection: Optional[Tuple[str]]=None,
+        distinct_on: Optional[Tuple[str]]=None,
+        parent_id: Optional[Tuple[str]]=None,
+        **kwargs
+    ) -> 'QueryResult':
+        _query = self._mount_query(
+            filters=self._process_filters(kwargs),
+            parent_id=parent_id,
+            projection=projection,
+            order_by=order_by,
+            distinct_on=distinct_on,
+        )
+        return QueryResult(_query, self.entity_instance)
 
-    def _process_filters(self, filter_dict: Dict) -> Tuple:
+    def _process_filters(self, filter_dict: Dict) -> Tuple[str, str, Any]:
         OPERATIONS_TO_QUERY = {
             "eq": "=",
             "gt": ">",
@@ -71,10 +112,6 @@ class Query:
             "le": "<="
         }
         DEFAULT_QUERY_OPERATION = "eq"
-
-        OPERATIONS_TO_VALIDATE = {
-            "in": None
-        }
 
         result = []
         for key, value in filter_dict.items():
