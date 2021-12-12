98da5385cc43874488120c93c71e2097d372b116
Geraldo Castro <victormatheuscastro@gmail.com>
2021-12-12 00:35:20 -0300

    Update version for testing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="noseiquela_orm",
-    version="0.0.4/dev5",
+    version="0.0.4/dev6",
     install_requires=[
         "python-dateutil~=2.8",
         "google-cloud-datastore~=2.3",
