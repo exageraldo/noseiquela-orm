fb506f4853b29f8e74049966af96795fa065ffb7
Geraldo Castro <victormatheuscastro@gmail.com>
2021-12-11 23:05:02 -0300

    Add github action to release package to pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="noseiquela_orm",
-    version="0.0.4-dev",
+    version="0.0.4/dev1",
     install_requires=[
         "python-dateutil~=2.8",
         "google-cloud-datastore~=2.3",
