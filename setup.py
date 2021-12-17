ce432a2c40f4d062b346758415eadfc628c400aa
Geraldo Castro <victormatheuscastro@gmail.com>
2021-12-16 23:09:27 -0300

    Improve filter to 'first' query method

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="noseiquela_orm",
-    version="0.0.7-dev2",
+    version="0.0.8-dev1",
     install_requires=[
         "python-dateutil~=2.8",
         "google-cloud-datastore~=2.3",
